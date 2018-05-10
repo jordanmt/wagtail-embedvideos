129b5cab57b0a3d0c936c7cb5dcd0407381a765e
Jordan Teichmann <jordan.mht@gmail.com>
2018-05-10 11:25:27 -0400

    BaseEmbedVideoChooserPanel made redundant by changes to Wagtail edit_handlers API

--- wagtail_embed_videos/edit_handlers.py
+++ wagtail_embed_videos/edit_handlers.py
@@ -3,21 +3,11 @@ from __future__ import absolute_import, unicode_literals
 from wagtail.admin.edit_handlers import BaseChooserPanel
 from .widgets import AdminEmbedVideoChooser
 
-
-class BaseEmbedVideoChooserPanel(BaseChooserPanel):
+class EmbedVideoChooserPanel(BaseChooserPanel):
     object_type_name = "embed_video"
 
-    @classmethod
     def widget_overrides(cls):
-        return {cls.field_name: AdminEmbedVideoChooser}
-
-
-class EmbedVideoChooserPanel(object):
-    def __init__(self, field_name):
-        self.field_name = field_name
-
-    def bind_to_model(self, model):
-        return type(str('_EmbedVideoChooserPanel'), (BaseEmbedVideoChooserPanel,), {
-            'model': model,
-            'field_name': self.field_name,
-        })
+        return {
+            cls.field_name: AdminEmbedVideoChooser
+        }
+    

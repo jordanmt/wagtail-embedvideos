5981cda911e3f4939d0939e77c7aa48b4587dabc
Jordan Teichmann <jordan.mht@gmail.com>
2020-04-27 22:06:33 -0400

    more wagtail.admin.utils reorganization

--- wagtail_embed_videos/views/embed_videos.py
+++ wagtail_embed_videos/views/embed_videos.py
@@ -11,9 +11,8 @@ from django.http import HttpResponse
 
 from wagtail.admin.forms.search import SearchForm
 from wagtail.admin import messages
-from wagtail.admin.utils import (
-    PermissionPolicyChecker, permission_denied, popular_tags_for_model
-)
+from wagtail.admin.auth import PermissionPolicyChecker, permission_denied
+from wagtail.admin.models import popular_tags_for_model
 from wagtail.search.backends import get_search_backends
 
 from wagtail_embed_videos.models import get_embed_video_model
